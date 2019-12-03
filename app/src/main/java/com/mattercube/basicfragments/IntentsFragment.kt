6d2c6a6cad2c248ce3fb39eb449168371374cb1d
MaTTerCuBe'exit <rkhamov@msudenver.edu>
2019-12-03 14:10:40 -0700

    Changed layout of intents fragment to be scrollable (just in case), and added a preliminary title and description.

--- app/src/main/java/com/mattercube/basicfragments/IntentsFragment.kt
+++ app/src/main/java/com/mattercube/basicfragments/IntentsFragment.kt
@@ -7,9 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class IntentsFragment : Fragment() {
 
     override fun onCreateView(
