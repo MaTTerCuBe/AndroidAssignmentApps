ec8e795e909ce6a6581d55f991c32bfab04d7567
MaTTerCuBe'exit <rkhamov@msudenver.edu>
2019-12-05 12:50:14 -0700

    Intents are succesfully changing sample text, began working on functions to simplify code.

--- app/src/main/java/com/mattercube/basicfragments/IntentsFragment.kt
+++ app/src/main/java/com/mattercube/basicfragments/IntentsFragment.kt
@@ -1,13 +1,18 @@
 package com.mattercube.basicfragments
 
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import kotlinx.android.synthetic.main.fragment_intents.*
+import android.widget.Toast
 
 private const val ACTION_RETRIEVE_FONT = "com.mattercube.basicfragments.RETRIEVE_FONT"
 private const val EXTRA_FONT_VALUE = "com.mattercube.basicfragments.FONT_VALUE"
@@ -15,10 +20,12 @@ private const val EXTRA_FONT_VALUE = "com.mattercube.basicfragments.FONT_VALUE"
 class IntentsFragment : Fragment() {
 
     // defining our text views
-    private var sampleOne: TextView? = null
+    //val sampleOne: TextView? = view?.findViewById(R.id.sample_one_view)
     private var sampleTwo: TextView? = null
 
-    private var fontValue = 0;  // The companion App will change this value, and result in different fonts
+    private var fontValue: Int? = 0;  // The companion App will change this value, and result in different fonts
+
+    private var viewChosen: Int? = 0    // Which view's button was pressed
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -31,8 +38,10 @@ class IntentsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        val defaultFace = android.graphics.Typeface.DEFAULT
+
         // Assigning TextViews by finding them in our XML layout
-        sampleOne = view?.findViewById(R.id.sample_one_view)
+        //val sampleOne: TextView? = view?.findViewById(R.id.sample_one_view)
         sampleTwo = view?.findViewById(R.id.sample_two_view)
 
         // Setting request code values, these will be used with each text view.
@@ -40,21 +49,77 @@ class IntentsFragment : Fragment() {
         val REQUEST_TWO = 2
 
         // Setting onClickListeners
-        sampleOne?.setOnClickListener {
+        sample_one_view?.setOnClickListener {
+
+            Log.i("Intent check", "TextView one pressed")
             changeFont(REQUEST_ONE)
+            viewChosen = 1
+
         }
 
         sampleTwo?.setOnClickListener {
+            changeFont(REQUEST_TWO)
+            //
+        }
 
+        fun applyFont(fontCode: Int, viewCode: TextView) {
+            when (fontCode == 1) {
+
+            }
         }
 
     }
+
     // Function to send our intent
     fun changeFont(requestCode: Int) {
 
         val fontIntent = Intent(ACTION_RETRIEVE_FONT)
-        fontIntent.putExtra(EXTRA_FONT_VALUE, fontValue)
+        //fontIntent.putExtra(EXTRA_FONT_VALUE, fontValue)
 
         startActivityForResult(fontIntent, requestCode)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        Log.i("Intent check", "in Activity Result")
+
+        fontValue = data?.getIntExtra("FONT_VALUE", 0)
+
+        Log.i("Intent check", "fontValue should be 1, actually is: $fontValue")
+
+        if (viewChosen == 1) {
+
+            if (fontValue == 1) {
+
+                Log.i("Intent check", "In if block for value of 1")
+
+                applyFont2(sample_one_view)
+                //sample_one_view.setTypeface(null, Typeface.BOLD)
+                //sampleOne?.setTypeface(sampleOne.typeface, Typeface.MONOSPACE)
+                //sample_one_view.setTextColor(resources.getColor(R.color.not_approved))
+            }
+
+            else if (fontValue == 2) {
+
+                Log.i("Intent check", "In if block for value of 1")
+
+                applyFont2(sample_one_view)
+                //sample_one_view.setTypeface(null, Typeface.BOLD)
+                //sampleOne?.setTypeface(sampleOne.typeface, Typeface.MONOSPACE)
+                //sample_one_view.setTextColor(resources.getColor(R.color.not_approved))
+            }
+        }
+    }
+
+    fun applyFont2(viewCode: TextView?, fontCode: Int) {
+
+
+            //viewCode.typeface = Typeface.DEFAULT
+        viewCode?.setTypeface(viewCode.typeface, Typeface.BOLD)
+        viewCode?.setTextColor(resources.getColor(R.color.not_approved))
+            //viewCode.setTypeface(viewCode.null, android.graphics.Typeface.BOLD)
+            //viewCode.setTypeface(null, 2)
+
     }
 }
